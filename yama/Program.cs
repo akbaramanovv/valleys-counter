87e01c5e585e9bd47d1de46613d3e0c9203a2eed
akbaramanovv <46891188+akbaramanovv@users.noreply.github.com>
2019-05-10 16:27:56 +0400

    Add files via upload

---
+++ yama/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        private static object arr;
+
+        static void Main(string[] args)
+        {
+              
+             string steps = Console.ReadLine();
+             string[] arr = steps.Split(' ');
+
+            Console.WriteLine("Yamalarin sayi :" + volleys(arr).ToString());
+            Console.ReadKey();
+        }
+
+        static int volleys(string [] voll)
+        {
+            int level = 0;
+            bool inVolley = false;
+            int vollCount = 0;
+
+            // U D D U D D D U D U U U U D D U D U
+            for (int i = 0; i < voll.Length; i++)
+            {
+                if (voll[i] == "U") { level++; }
+                else { level--; }
+
+                if(level < 0)
+                {
+                    inVolley = true;
+                }
+                else if(level == 0 && inVolley == true)
+                {
+                    vollCount++;
+                    inVolley = false; 
+                }
+
+            }
+            return vollCount;
+        }
+    }
+}
